20d15f38f4c47184a6e2b83205b158549d3b6918
jnytnai0613 <surf.0530.junya@gmail.com>
2025-07-02 00:35:31 +0900

    bump: dotfiles update at 2025-07-02 update setup.zsh for safety

--- setup.zsh
+++ setup.zsh
@@ -1,6 +1,17 @@
 #!/bin/zsh
 
+if ! which brew; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
 brew install aqua
 brew install awscli
 brew install tree
 brew install wget
+
+if [ -e ~/.zshrc ]; then
+    cp ~/.zshrc ~/.zshrc.bak
+    echo '######################################'
+    echo 'Backup of ~/.zshrc as ~/.zshrc.bak'
+    echo '######################################'
+fi
