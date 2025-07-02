1822c571f9c49852fcc17b23f682592b754ce5c0
Junya Taniai <31709999+jnytnai0613@users.noreply.github.com>
2025-07-02 10:55:09 +0900

    bump: dotfiles update at 2025-07-02 (#15)

--- install.zsh
+++ install.zsh
@@ -1,10 +1,21 @@
 #!/bin/zsh
 
-mkdir -p ~/.config/aqua
-mkdir ~/.aws
+if [ ! -e ~/.config ]; then
+  mkdir ~/.config
+fi
+
+if [ ! -e ~/.config/aqua ]; then
+  mkdir ~/.config/aqua
+fi
+
+if [ ! -e ~/.aws ]; then
+  mkdir ~/.aws
+fi
+
 ln -sf $(pwd)/.zshrc ~/.zshrc
 ln -sf $(pwd)/.config/starship.toml ~/.config/starship.toml
 ln -sf $(pwd)/.config/aqua/aqua.yaml ~/.config/aqua/aqua.yaml
 ln -sf $(pwd)/changelog.config.js ~/changelog.config.js
 ln -sf $(pwd)/.aws/config ~/.aws/config
 ln -sf $(pwd)/.aws/credentials ~/.aws/credentials
+ln -sf $(pwd)/.Brewfile ~/.Brewfile
