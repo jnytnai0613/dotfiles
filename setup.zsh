a8a62fb1e54b9db0dadb31dcd47d9c5b4dffd667
Junya Taniai <31709999+jnytnai0613@users.noreply.github.com>
2025-07-02 11:53:19 +0900

    bump: dotfiles update at 2025-07-02 Add zsh plugin (#18)

--- setup.zsh
+++ setup.zsh
@@ -5,8 +5,12 @@ if ! which brew; then
 fi
 
 brew bundle --global --file=~/.Brewfile
-
 npm install -g git-cz
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 if [ -e ~/.zshrc ]; then
     cp ~/.zshrc ~/.zshrc.bak
